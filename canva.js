773071b12a523745586473cccce5f761acbf4695
kromate <akpananthony33@gmail.com>
2022-05-25 16:22:59 +0100

    init

---
+++ canva.js
@@ -0,0 +1,10 @@
+const canvas = document.querySelector('canvas')
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+const c = canvas.getContext('2d')
+
+c.fillRect(100, 100,100,100)
+c.fillRect(200, 300,100,100)
+c.fillRect(300, 500,100,100)
